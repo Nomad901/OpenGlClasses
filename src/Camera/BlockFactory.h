23b68a8416f2e3a1025ffd3c9b81cf6a8b2654ae
Tango <maxim.ant07@gmail.com>
2025-08-07 15:40:43 +0300

    Update BlockFactory.h

--- src/Camera/BlockFactory.h
+++ src/Camera/BlockFactory.h
@@ -2,17 +2,35 @@
 #include <vector>
 #include <array>
 #include <span>
+#include <algorithm>
 
 #include "SDL3/SDL.h"
 #include "glad/glad.h"
 
+#include "Figure.h"
 
 class BlockFactory
 {
 public:
+	BlockFactory();
 
+	void pushFigure(Figure::Type pType);
+	void pushFigures(Figure::Type pType, int32_t pNumber);
+	void popFigure();
+	void eraseFigure(size_t pIndex);
+	void setAllFiguresShown(bool pShown);
+
+	void setCurrent(size_t pCurrentNum);
+	size_t getCurrentNum() const noexcept;
+
+	Figure& getFigure(size_t pIndex) noexcept;
+	std::vector<Figure>& getStorage() noexcept;
+
+	void render();
 
 private:
+	size_t mCurrentNum{};
+	std::vector<Figure> mFigures;
 
 };
 
