b701f7551aafe42a0d462e6fe01b5bc5a7eecebd
Tango <maxim.ant07@gmail.com>
2025-08-07 15:40:55 +0300

    Update Figure.cpp

--- src/Camera/Figure.cpp
+++ src/Camera/Figure.cpp
@@ -6,6 +6,13 @@ Figure::Figure()
 	mIndexes.reserve(100);
 }
 
+Figure::~Figure()
+{
+	glDeleteBuffers(1, &mVertexBufferObject);
+	glDeleteBuffers(1, &mIndexBufferObject);
+	glDeleteVertexArrays(1, &mVertexArrayObject);
+}
+
 auto Figure::initTriangle() -> void
 {
 	mTypeFigure = Type::TRIANGLE;
@@ -19,12 +26,11 @@ auto Figure::initTriangle() -> void
 
 	mVertexes =
 	{
-		// Front face 
-		-mWidth, -mHeight,  0.5f,
+		-mWidth+mMultiplier, -mHeight, -1.0f,
 		 mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
-		 mWidth, -mHeight,  0.5f,
+		 mWidth + mMultiplier, -mHeight, -1.0f,
 		 mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
-		-mWidth,  mHeight,  0.5f,
+		-mWidth + mMultiplier,  mHeight, -1.0f,
 		 mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
 	};
 
@@ -76,14 +82,13 @@ auto Figure::initQuad() -> void
 
 	mVertexes =
 	{
-		// Front face 
-		-mWidth, -mHeight,  0.5f,
+		-mWidth + mMultiplier, -mHeight, -1.0f,
 		 mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
-		 mWidth, -mHeight,  0.5f,
+		 mWidth + mMultiplier, -mHeight, -1.0f,
 		 mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
-		-mWidth,  mHeight,  0.5f,
+		-mWidth + mMultiplier,  mHeight, -1.0f,
 		 mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
-		 mWidth,  mHeight,  0.5f,
+		 mWidth + mMultiplier,  mHeight, -1.0f,
 		 mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
 	};
 
@@ -91,6 +96,137 @@ auto Figure::initQuad() -> void
 	{
 		0, 1, 2,
 		2, 1, 3,
+	};
+
+	glCreateVertexArrays(1, &mVertexArrayObject);
+	glCreateBuffers(1, &mVertexBufferObject);
+
+	// vertex buffer
+	glNamedBufferData(mVertexBufferObject,
+					  mVertexes.size() * sizeof(GL_FLOAT),
+					  mVertexes.data(),
+					  GL_STATIC_DRAW);
+	//index buffer
+	glCreateBuffers(1, &mIndexBufferObject);
+	glNamedBufferData(mIndexBufferObject,
+					  mIndexes.size() * sizeof(GLuint),
+					  mIndexes.data(),
+					  GL_STATIC_DRAW);
+	glVertexArrayElementBuffer(mVertexArrayObject, mIndexBufferObject);
+
+	glVertexArrayVertexBuffer(mVertexArrayObject,
+							  0,
+							  mVertexBufferObject,
+							  0,
+							  6 * sizeof(GLfloat));
+
+	glEnableVertexArrayAttrib(mVertexArrayObject, 0);
+	glVertexArrayAttribFormat(mVertexArrayObject,
+							  0,
+							  3,
+							  GL_FLOAT,
+							  GL_FALSE,
+							  0);
+	glVertexArrayAttribBinding(mVertexArrayObject, 0, 0);
+
+	glEnableVertexArrayAttrib(mVertexArrayObject, 1);
+	glVertexArrayAttribFormat(mVertexArrayObject,
+							  1,
+							  3,
+							  GL_FLOAT,
+							  GL_FALSE,
+							  3 * sizeof(GLfloat));
+	glVertexArrayAttribBinding(mVertexArrayObject, 1, 0);
+}
+
+auto Figure::initCube() -> void
+{
+	mTypeFigure = Type::CUBE;
+
+	mColorsVertexes =
+	{
+		std::array<float,3>({ -1.0f, 0.0f, 0.0f, }),
+		std::array<float,3>({  0.0f, 1.0f, 0.0f, }),
+		std::array<float,3>({  0.0f, 0.0f, 1.0f, }),
+		std::array<float,3>({ -1.0f, 1.0f, 0.0f, })
+	};
+
+	mVertexes =
+	{
+	  -mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	   mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	  -mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	   mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+
+	  -mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	   mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	  -mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	   mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+
+	  -mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	  -mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	  -mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	  -mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+
+	   mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	   mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	   mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	   mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+
+	  -mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	   mWidth + mMultiplier,  mHeight, -1.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	  -mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	   mWidth + mMultiplier,  mHeight, -2.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+
+	  -mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[0][0], mColorsVertexes[0][1], mColorsVertexes[0][2],
+	   mWidth + mMultiplier, -mHeight, -2.0f,
+	   mColorsVertexes[1][0], mColorsVertexes[1][1], mColorsVertexes[1][2],
+	  -mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[2][0], mColorsVertexes[2][1], mColorsVertexes[2][2],
+	   mWidth + mMultiplier, -mHeight, -1.0f,
+	   mColorsVertexes[3][0], mColorsVertexes[3][1], mColorsVertexes[3][2],
+	};
+
+	mIndexes =
+	{
+		0, 1, 2,
+	    2, 1, 3,
+
+		4, 6, 5,
+		5, 6, 7,
+
+		8, 9, 10,
+		10, 9, 11,
+
+		12, 13, 14,
+		14, 13, 15,
+
+		16, 17, 18,
+		18, 17, 19,
+
+		20, 21, 22,
+		22, 21, 23
 	};
 
 	glCreateVertexArrays(1, &mVertexArrayObject);
@@ -107,7 +243,7 @@ auto Figure::initQuad() -> void
 		mIndexes.size() * sizeof(GLuint),
 		mIndexes.data(),
 		GL_STATIC_DRAW);
-	glVertexArrayElementBuffer(mVertexBufferObject, mIndexBufferObject);
+	glVertexArrayElementBuffer(mVertexArrayObject, mIndexBufferObject);
 
 	glVertexArrayVertexBuffer(mVertexArrayObject,
 		0,
@@ -191,6 +327,16 @@ auto Figure::getIndexes() noexcept -> const std::vector<int16_t>&
 	return mIndexes;
 }
 
+auto Figure::setMultiplier(int32_t pMultiplier) -> void
+{
+	mMultiplier = pMultiplier;
+}
+
+auto Figure::getMultyplier() const noexcept -> int32_t
+{
+	return mMultiplier;
+}
+
 auto Figure::setOffset(float pOffset) -> void
 {
 	mOffset = pOffset;
@@ -213,21 +359,22 @@ auto Figure::getRotation() const noexcept -> float
 
 auto Figure::render() -> void
 {
-	// enable our attributes
+	if (!mFigureIsShown) return;
+
 	glBindVertexArray(mVertexArrayObject);
-	// render the data. 0 the beginning of the array, 3 - the end
 	if (mFigureIsShown)
 	{
 		if (mTypeFigure == Type::TRIANGLE)
 		{
-			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glDrawArrays(GL_TRIANGLES, 0, 3);
 		}
 		else
 		{
 			glDrawElements(GL_TRIANGLES,
-						   mIndexes.size() * sizeof(GLuint),
-						   GL_UNSIGNED_INT,
+						   mIndexes.size(),
+						   GL_UNSIGNED_SHORT,
 						   0);
 		}
 	}
+	glBindVertexArray(0);
 }
