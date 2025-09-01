b2b412624b7a56fd0585d1f01392789c42d3eb4d
Nomad901 <maxim.ant07@gmail.com>
2025-09-01 21:13:38 +0300

    batch rendering

--- src/src/2d Image/src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Program.h"
-
-int main(int argc, char* argv[])
-{
-	// type of the mesh / screen width / screen height
-	Program program(Program::typeMesh::QUAD_IMG, 1280, 720);
-	program.run();
-}
--- src/src/StructuredAndReady_program/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "Program.h"
-
-int main(int argc, char* argv[])
-{
-	// type of the mesh / screen width / screen height
-	Program program(Program::typeMesh::TRIANGLE, 1280, 720);
-	program.run();
-
-}
