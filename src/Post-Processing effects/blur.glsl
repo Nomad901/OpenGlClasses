a2bcb88ff688334e5cc452f04d89bf8eb7d2f724
Tango <maxim.ant07@gmail.com>
2025-09-21 14:42:39 +0300

    Create blur.glsl

---
+++ src/Post-Processing effects/blur.glsl
@@ -0,0 +1,45 @@
+#version 440
+
+in vec2 fragPosTex;
+
+out vec4 outColor;
+
+uniform sampler2D uTexture;
+uniform float surroundNum;
+uniform float insideNum;
+
+const float offset = 1.0f / 300.0f;
+
+void main()
+{
+	vec2 offsets[9] = vec2[](
+		vec2(-offset, offset),
+		vec2(   0.0f, offset),
+		vec2( offset, offset),
+		vec2(-offset,   0.0f),
+		vec2(   0.0f,   0.0f),
+		vec2( offset,   0.0f),
+		vec2(-offset, -offset),
+		vec2(   0.0f, -offset),
+		vec2( offset, -offset)
+	);
+
+	float kernel[9] = float[](
+		surroundNum / 16, surroundNum / 16, surroundNum / 16,
+		surroundNum / 16, insideNum / 16, surroundNum / 16,
+		surroundNum / 16, surroundNum / 16, surroundNum / 16
+	);
+	
+	vec3 sampleTex[9];
+	for(int i = 0; i < 9; ++i)
+	{
+		sampleTex[i] = vec3(texture(uTexture, fragPosTex.st + offsets[i]));
+	}
+	vec3 col = vec3(0.0f);
+	for(int i = 0; i < 9; ++i)
+	{
+		col += sampleTex[i] * kernel[i];
+	}
+
+	outColor = vec4(col, 1.0f);
+}
